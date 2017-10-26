5138f169b2c5603890ed4077d13040d17deb3f9a
youtangai <youtanagai@gmail.com>
2017-10-26 21:44:35 +0900

    implement D&D

--- js/animate.js
+++ js/animate.js
@@ -1,7 +1,28 @@
+var clicking = false;
+
 $(function() {
-    $('#box1').mousedown(MouseDown)
+    $('.box').mousedown(MouseDown);
+    $(document).mouseup(MouseUp);
+    $('.box').mousemove(MouseMove);
 });
 
 function MouseDown(){
-    alert("mousdonw");
+    clicking = true;
+    $('#clickstatus').text("mousedown");
+    $(this).addClass("drag");
+}
+
+function MouseUp(){
+    clicking = false;
+    $('#clickstatus').text("mouseup");
+    $('#movestatus').text("released");
+    $('.drag').removeClass("drag")
+}
+
+function MouseMove(event){
+    if (!clicking) return;
+    var drag = $('.drag');
+    var offset = drag.offset();
+    $('#movestatus').text("mouse moving  " + event.pageX + "," + event.pageY + " offset" + offset.left + "," + offset.top);
+    drag.offset({top: event.pageY-45, left: event.pageX-45})
 }
