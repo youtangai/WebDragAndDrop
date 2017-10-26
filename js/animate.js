54b6bd9bff3cc206fd0647fec8f2fbf9967c330c
youtangai <youtanagai@gmail.com>
2017-10-26 18:23:59 +0900

    set up three box

--- js/animate.js
+++ js/animate.js
@@ -1,16 +1,3 @@
 $(function() {
-    $('#box1')
-        .animate({'left': '500px'})
-        .animate({'top': '100px'})
-        .animate({'left': '10px'})
-        .animate({'left': '100px'});
-    $('#box2').animate({
-        'top': '500px'
-    }, {
-        'duration':600,
-        'easing':'linear',
-        'complete': function(){
-            alert("終わりました");
-        }
-    });
+    
 });
